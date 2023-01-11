7d8c17f405df538680ffdaace9b8e3036c88123c
Patrick Chou <99582012+Trickslolll@users.noreply.github.com>
2023-01-11 15:35:02 +0800

    Update function.py

--- function.py
+++ function.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup
 def check_status(url):  # check if there is item on sale on the website
     target = requests.get(url)
     soup = BeautifulSoup(target.text, "html.parser")
-    result = soup.find_all("div", class_="as-price-currentprice as-producttile-currentprice")
+    # as-price-currentprice as-producttile-currentprice
+    result = soup.find_all("div", class_="desired_class_here")
 
     if len(result) == 0:
         return False
